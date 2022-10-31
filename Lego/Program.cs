72995eef3eef3cfa7fa7d50ed66d65053d7cf992
Szymon Rogoziński <pokeszymon.pl@gmail.com>
2022-10-31 21:16:33 +0100

    fdfgf

--- Lego/Program.cs
+++ Lego/Program.cs
@@ -6,16 +6,63 @@ namespace Lego
     {
         static void Main(string[] args)
         {
-            Character c = new Character("Skate kid",
-                new Brick("Czerwona czapka"),
-                new Brick("Żółta głowa"),
-                new Brick("Koszula w kratę"),
-                new Brick("Niebieskie spodnie"));
+            Playground();
+        }
 
-            Console.WriteLine(c);
-            Console.Beep();
+        private static void Playground()
+        {
+            Character cavewoman = new Character("Pani jaskiniowiec",
+                new Brick("Bujna fryzura z kością"),
+                new Brick("Żółta zdziwiona głowa"),
+                new Brick("Koszulka ze skór zwierzęcych"),
+                new Brick("Spodnie ze skór zwierzęcych"));
+
+            Character wizard = new Character("Czarodziej",
+                new Brick("Czapka Czarodzieja"),
+                new Brick("Żółta poważna twarz"),
+                new Brick("Długa broda"),
+                new Brick("Szara koszulka"),
+                new Brick("Szare spodnie"));
+
+            Character panda = new Character("Kobieta w stroju pandy",
+                new Brick("Długie włosy"),
+                new Brick("Uśmiechnięta twarz"),
+                new Brick("Kostium pandy: koszulka"),
+                new Brick("Kostium pandy: spodnie"));
+
+            Character liberty = new Character("Statua wolności",
+                new Brick("Korona"),
+                new Brick("Zielona twarz"),
+                new Brick("Długa suknia: góra"),
+                new Brick("Długa suknia: dół"));
+
+            Console.WriteLine(cavewoman);
+            Console.WriteLine();
             Console.ReadKey();
-            Console.WriteLine(c.InLeftHand);
+
+            Console.WriteLine(wizard);
+            Console.WriteLine();
+            Console.ReadKey();
+
+            Console.WriteLine(panda);
+            Console.WriteLine();
+            Console.ReadKey();
+            panda.top_head = new Brick("Maska pandy");
+            Console.WriteLine(panda);
+            Console.WriteLine();
+            Console.ReadKey();
+
+            Console.WriteLine(liberty);
+            Console.WriteLine();
+            Console.ReadKey();
+            liberty.InRightHand = new Brick("Pochodnia");
+            Console.WriteLine();
+            Console.ReadKey();
+
+            cavewoman.WaveHand();
+            wizard.WaveHand();
+            panda.WaveHand();
+            liberty.WaveHand();
             Console.ReadKey();
         }
 
@@ -79,7 +126,8 @@ namespace Lego
             }
         }
 
-        public Character(string name, Brick top_head, Brick head, Brick between_head_and_torso, Brick arms_and_torso, Brick legs)
+        public Character(string name, Brick top_head, Brick head, 
+            Brick between_head_and_torso, Brick arms_and_torso, Brick legs)
         {
             this.name = name;
             this.top_head = top_head;
@@ -94,7 +142,6 @@ namespace Lego
             this.name = name;
             this.top_head = top_head;
             this.head = head;
-            between_head_and_torso = null;
             this.arms_and_torso = arms_and_torso;
             this.legs = legs;
         }
